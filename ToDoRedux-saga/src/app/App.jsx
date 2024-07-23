e911b5b6f472252fe9f0fcf25debcdd4e5d665e4
Anton Tsyhanok <anton160400@gmail.com>
2024-07-23 14:36:25 +0300

    fix remove useEffect in app

--- ToDoRedux-saga/src/app/App.jsx
+++ ToDoRedux-saga/src/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { routes } from '../config/routes';
 import Main from '../pages/Main';
